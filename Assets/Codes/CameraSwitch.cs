ccf0027092b5e422622a3d9200d08485e76a89a2
teago83 <thyagodesouzapereira@gmail.com>
2020-08-20 18:44:58 -0300

    Algumas modificações simples.
    Fora adicionado o soldado para ser controlado pelo jogador, alguns assets além desse também foram importados. Por fim, fora adicionado as câmeras e o sistema de troca de câmeras (que precisa ser testado).

---
+++ Assets/Codes/CameraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public GameObject CameraOne;
+    public GameObject CameraTwo;
+
+    // Update is called once per frame
+    
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CameraOne.SetActive(true);
+            CameraTwo.SetActive(false);
+        }
+    }
+
+    
+}
