5ae9fd23c6071ac6c26c2744afa0ee4828cf4693
teago83 <thyagodesouzapereira@gmail.com>
2020-09-06 15:13:57 -0300

    Sistema de interação adicionado.
    Adição de um sistema de interações básico e de uma interação com a porta dupla no fim da área principal.

---
+++ Assets/Scripts/Other Stuff/Object-Related/DoorControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public bool IsOpen;
+    public Animator animator;
+    public GameObject TheDoors;
+
+    public void OpenDoor()
+    {
+        if (!IsOpen)
+        {
+            IsOpen = true;
+            Debug.Log("The door is now open!");
+            // animator.SetBool("IsOpen", IsOpen);
+
+            TheDoors.GetComponent<Animator>().Play("OpenDoubleDoor");
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Other Stuff/Object-Related/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+
+    public bool IsInRange;
+    public KeyCode InteractKey;
+    public UnityEvent Interaction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsInRange)
+        {
+            if (Input.GetKeyDown(InteractKey)) // If the player presses the interact key...
+            {
+                Interaction.Invoke(); // ...the event is triggered.
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsInRange = true;
+            Debug.Log("The player is now in range.");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsInRange = false;
+            Debug.Log("The player is not in range anymore.");
+        }
+    }
+}
