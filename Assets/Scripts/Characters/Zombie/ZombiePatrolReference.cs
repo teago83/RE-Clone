6923eaf4261d5a9629a2f06ebc2d4670affbae33
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-29 22:16:45 -0300

    Adição de uma base para a movimentação da AI do zumbi.
    Um script base para ser usado como referência no estado de patrulha da Finite State Machine do zumbi foi adicionado. Referência do vídeo usado como base para a criação do script está em um comentário no fim do script.

---
+++ Assets/Scripts/Characters/Zombie/ZombiePatrolReference.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiePatrolReference : MonoBehaviour
+{
+    public float speed;
+    public float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+
+    private int randomSpot;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance (transform.position, moveSpots[randomSpot].position) < .2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+}
+
+
+
+// TUTORIAL USED AS A BASIS FOR CREATING THIS SCRIPT:
+// https://youtu.be/8eWbSN2T8TE
