40ab9807dc4b624b34584239b7f529c519dcfbfd
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-10-03 17:28:37 -0300

    Melhoria na detecção do jogador pelo zumbi.
    Agora, o zumbi começa a seguir o jogador caso ele esteja na área de detecção do zumbi, mas para de segui-lo caso ele saia do campo de visão do zumbi, não somente da área de detecção.

--- Assets/Scripts/Characters/Zombie/Detector.cs
+++ Assets/Scripts/Characters/Zombie/Detector.cs
@@ -8,12 +8,6 @@ public class Detector : MonoBehaviour
     public bool IsInRange;
     public UnityEvent Detection;
 
-    // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -21,17 +15,6 @@ public class Detector : MonoBehaviour
             IsInRange = true;
             Debug.Log("The zombie is seeing the player.");
             Detection.Invoke();
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            IsInRange = false;
-            Debug.Log("The zombie isn't seeing the player.");
-            /* Find a way of leaving the combat state after the player
-             * isn't in the zombie's range anymore */
         }
     }
 }
---
+++ Assets/Scripts/Characters/Zombie/Viewer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Viewer : MonoBehaviour
+{
+    public bool IsSeeing;
+    /* you'll need to find a way of making a condition for the zombie
+     * to be shot by the player, making the zombie start to follow
+     * the player while it can see the player */
+    public UnityEvent NotSeeing;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsSeeing = false;
+            Debug.Log("The zombie isn't seeing the player.");
+            NotSeeing.Invoke();
+        }
+    }
+}
