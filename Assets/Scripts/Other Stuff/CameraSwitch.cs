eb86020dc86f12b3e71c8a2d1a4de6985159052c
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-10-10 12:07:45 -0300

    Animação de transição de cenas adicionada.
    Resta fazer a transição funcionar.

--- Assets/Scripts/Other Stuff/CameraSwitch.cs
+++ Assets/Scripts/Other Stuff/CameraSwitch.cs
@@ -7,6 +7,9 @@ public class CameraSwitch : MonoBehaviour
 
     public GameObject CameraTURN_ON;
     public GameObject CameraTURN_OFF;
+    public static GameObject LastActiveCamera; 
+    // This object is used so that the DoorControl script can set the LastActiveCamera off
+    // when it instantiates the SceneTransition object. 
 
     void OnTriggerEnter (Collider other)
     {
@@ -14,6 +17,7 @@ public class CameraSwitch : MonoBehaviour
         {
             CameraTURN_ON.SetActive(true);
             CameraTURN_OFF.SetActive(false);
+            LastActiveCamera = CameraTURN_ON;
         }
     }
 
