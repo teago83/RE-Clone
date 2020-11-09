e961c40cdd83edcbb47e6d9ea46474f1b1f0507d
Dullkeerr <DuKeDeBuGGer@users.noreply.github.com>
2020-11-09 19:08:03 -0300

    Zombie code removed

--- Assets/Scripts/Characters/Player/FSM_scripts/PlayerStates/PlayerShootingState.cs
+++ Assets/Scripts/Characters/Player/FSM_scripts/PlayerStates/PlayerShootingState.cs
@@ -17,37 +17,7 @@ public class PlayerShootingState : PlayerBaseState
 
     public override void Update(PlayerFSM Player)
     {
-        //if (Player.ShootingCooldown <= 0f)
-        //{
-        //    if (Player.Weapons[PlayerFSM.CurrentWeapon].CurrentAmmo > 0)
-        //    {
-        //        if (Physics.Raycast(Player.WeaponBullets[PlayerFSM.CurrentWeapon].transform.position, Player.WeaponBullets[PlayerFSM.CurrentWeapon].transform.forward, out Player.HitInfo, Player.Weapons[PlayerFSM.CurrentWeapon].ShootingRange))
-        //        {
-        //            Debug.Log(Player.HitInfo.transform.name);
-        //            ZombieBehaviourFSM Zombie = Player.HitInfo.transform.GetComponent<ZombieBehaviourFSM>();
-        //            if (Zombie != null)
-        //            {
-        //                Zombie.ReallyHitByPlayer();
-        //            }
-        //        }
-        //        Player.Weapons[PlayerFSM.CurrentWeapon].CurrentAmmo -= 1;
-        //        Player.animComp.Play(Player.Weapons[PlayerFSM.CurrentWeapon].FiringAnimation);
-        //        Player.ShootingCooldown = Player.Weapons[PlayerFSM.CurrentWeapon].ShootingCooldown;
-        //        Player.FiringAnimationCooldown = Player.Weapons[PlayerFSM.CurrentWeapon].FiringAnimationCooldown;
 
-        //        if (PlayerFSM.CurrentWeapon == 0) 
-        //        {
-        //            Player.FiringPistolSFX.Play();
-        //            Player.PistolGunshot.Play();
-        //        }
-        //        else if (PlayerFSM.CurrentWeapon == 1)
-        //        {
-        //            Player.FiringShotgunSFX.Play();
-        //            Player.ShotgunGunshot.Play();
-        //        }
-        //        Debug.Log("Current " + Player.Weapons[PlayerFSM.CurrentWeapon].Name + " ammo: " + Player.Weapons[PlayerFSM.CurrentWeapon].CurrentAmmo);
-        //    }
-        //}
         if (Player.FiringAnimationCooldown > 0f)
         {
             Player.FiringAnimationCooldown -= Time.deltaTime;
