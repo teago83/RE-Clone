f6afcb103411344ad83f7617f7521e7230e10ae4
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-11-21 19:02:10 -0300

    Correção das texturas dos cilindros da cena 3 e algumas adições e melhorias.
    As texturas dos cilindros da cena 3 foram atualizadas, utilizando umas pré-feitas da asset store. Além disso, a imagem estática das câmeras foi atualizada, tendo um vídeo de estática real no fundo, dando um efeito mais legal, assim como agora tem-se uma interação com a TV, que liga e desliga, ficando com uma estática e um som de estática quando ligada, junto de um botão que é vermelho quando a TV está desligada e verde quando está ligada.

---
+++ Assets/Scripts/Other Stuff/Object-Related/TV.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour
+{
+    public GameObject TVScreen;
+    public GameObject StaticNoise;
+    public GameObject OnButton;
+    public GameObject OffButton;
+
+    private bool TVOn;
+
+    public void PressButton()
+    {
+        if (TVOn)
+        {
+            TurnOff();
+            return;
+        }
+            
+        if (!TVOn)
+        {
+            TurnOn();
+            return;
+        }
+            
+    }
+
+    private void TurnOn()
+    {
+        TVScreen.SetActive(true);
+        StaticNoise.SetActive(true);
+        OnButton.SetActive(true);
+        OffButton.SetActive(false);
+        TVOn = true;
+    }
+    private void TurnOff()
+    {
+        TVScreen.SetActive(false);
+        StaticNoise.SetActive(false);
+        OnButton.SetActive(false);
+        OffButton.SetActive(true);
+        TVOn = false;
+    }
+}
